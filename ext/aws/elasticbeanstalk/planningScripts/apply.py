a341330818031ca208501a9a34c54c5e4f2077d0
Benoit Moussaud <bmoussaud@xebialabs.com>
2020-01-29 17:14:55 +0100

    cleanup

--- ext/aws/elasticbeanstalk/planningScripts/apply.py
+++ ext/aws/elasticbeanstalk/planningScripts/apply.py
@@ -20,51 +20,41 @@ def stringify(stream):
     return out.toString()
 
 
-def stitchGlobalTemplate(deployed, context):
+def empty_json_file():
+    localOpts = LocalConnectionOptions(os=OperatingSystemFamily.UNIX)
+    host = OverthereHost(localOpts)
+    session =  OverthereHostSession(host)  
+    input_file = session.upload_text_content_to_work_dir(json.dumps(dict()),"empty_json_file.json",executable=False)
+    print ("input_file is {0}".format(input_file))
+    return input_file
+   
+
+def stitchGlobalTemplate(input_file, context):
 
     transformerContext = {
             "templateNamespace": "xld-aws-beanstalk-global",
             "templateName": "xld-aws-beanstalk-template-global",
             "format":"json"}
 
-    localOpts = LocalConnectionOptions(os=OperatingSystemFamily.UNIX)
-    host = OverthereHost(localOpts)
-    session =  OverthereHostSession(host)
-
-    data = {'services' : {
-        'db': {'image': 'mysql:123'}, 
-        'web': {'image': 'apache:34'}
-        } }
-    data = dict()
-    data_str = json.dumps(data)
-    print data_str
-
-
-    input_file = session.work_dir_file('aws_aws.txt')
-    input_file = session.upload_text_content_to_work_dir(data_str,"aws_aws.json",executable=True)
-    print "input_file is {0}".format(input_file)
-    #input_file = deployed.file
-
+    
     stitchedContent = context.getArtifactTransformer("stitchEngine").transform(input_file.getInputStream(), transformerContext)
 
     return stringify(stitchedContent)
 
 
-if deployed:
 
-    data = stitchGlobalTemplate(deployed, context)
+data = stitchGlobalTemplate(empty_json_file(), context)
 
-    print "--- OUTPUT ---"
-    print data
-    print "--- /OUTPUT ---"
+print ("--- OUTPUT ---")
+print (data)
+print ("--- /OUTPUT ---")
 
-
-    context.addStep(steps.os_script(
-        description="Deploy {0} to {1} Elastic BeanStalk".format(deployed.name, deployed.container.name),
-        target_host=deployed.container.account.awsHost,
-        script="aws/elasticbeanstalk/deploy",
-        freemarker_context={"deployedApplication": deployedApplication, 
-            "stack_name": "{0}-{1}-{2}-{3}".format(deployedApplication.environment.name,deployedApplication.version.application.name,deployed.name, deployed.container.name),
-            "operation" : delta.operation,
-            "data": data},
-        order=61))
+context.addStep(steps.os_script(
+    description="Deploy {0} to {1} Elastic BeanStalk".format(deployed.name, deployed.container.name),
+    target_host=deployed.container.account.awsHost,
+    script="aws/elasticbeanstalk/deploy",
+    freemarker_context={"deployedApplication": deployedApplication, 
+        "stack_name": "{0}-{1}-{2}-{3}".format(deployedApplication.environment.name,deployedApplication.version.application.name,deployed.name, deployed.container.name),
+        "operation" : delta.operation,
+        "data": data},
+    order=61))
