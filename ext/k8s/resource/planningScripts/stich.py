8b5096d984fab60ca76195017434e463ca9f846d
Benoit Moussaud <bmoussaud@xebialabs.com>
2020-02-10 10:32:02 +0100

    WIP

--- ext/k8s/resource/planningScripts/stich.py
+++ ext/k8s/resource/planningScripts/stich.py
@@ -2,16 +2,28 @@
 from stitch import stitchGlobalTemplate
 import json
 
-transformerContext = {
+mandatory_transformer_context ={
         "templateNamespace": "xld-k8s",
-        "templateName": "xld-k8s-resource-template",
+        "templateName": "xld-k8s-mandatory-template",
         "format":"json"}
 
-data = stitchGlobalTemplate(deployed, context, transformerContext)
+
+if deployedApplication.environment.enableStitch:
+
+    transformerContext ={
+            "templateNamespace": "xld-k8s",
+            "templateName": "xld-k8s-resource-template",
+            "format":"json"}
+
+    data = stitchGlobalTemplate(deployed, context, transformerContext)
+else:
+    data = stitchGlobalTemplate(deployed, context, mandatory_transformer_context)
+
+
+
 resourceName=json.loads(data)['metadata']['name']
 resource=json.loads(data)['kind']
 
 context.setAttribute("document",data)
 context.setAttribute("resourceName",resourceName)
 context.setAttribute("resource",resource)
-
