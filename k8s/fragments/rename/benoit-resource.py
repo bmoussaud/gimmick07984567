5d16ac36d4944e4a0270bd6cc51207a15989e96a
Benoit Moussaud <bmoussaud@xebialabs.com>
2020-02-11 10:17:17 +0100

    WIP

--- k8s/fragments/rename/benoit-resource.py
+++ k8s/fragments/rename/benoit-resource.py
@@ -6,6 +6,6 @@ def stitch(ctx, source):
     metadata_node = source.path("metadata")
     prefix = "benoit"
     new_name = "{0}-{1}".format(prefix,metadata_node.get("name").textValue())
-    #new_name = new_name.replace("-",".")
-    #metadata_node.put('name', new_name)
+    new_name = new_name.replace("-",".")
+    metadata_node.put('name', new_name)
     return source
